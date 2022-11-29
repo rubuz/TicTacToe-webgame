5b6778d7b12e2f84b55d5d512bb9a8e3822fb433
rubuz <rubuz23@gmail.com>
2022-11-29 11:55:37 +0100

    initial commit

---
+++ scripts/config.js
@@ -0,0 +1,35 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; // +"1" => 1
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayrname = formData.get("playername").trim();
+
+  if (!enteredPlayrname) {
+    // enteredPlayername === ""
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayrname;
+
+  players[editedPlayer - 1].name = enteredPlayrname;
+
+  closePlayerConfig();
+}
